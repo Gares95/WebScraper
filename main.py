8ed4c2e60a4205df7fccf9608e4d22a4c605c4e6
Guillermo <guillermo.garcia.espuela@gmail.com>
2020-09-08 23:54:47 +0200

    Version to obtain the Open values for each day. SAN

--- main.py
+++ main.py
@@ -1,36 +1,23 @@
-#clear()
-#del r
-#install bs4
+# for element in dir():
+#     if element[0:2] != "__":
+#         del globals()[element]
+# del element
 
 from bs4 import BeautifulSoup
 import requests
-# =============================================================================
-# #r = requests.get('https://finance.yahoo.com/quote/SAN/history?p=SAN')
-# #r.status_code
-# #b'Python is a programming language' in r.content
-# url = 'https://finance.yahoo.com/quote/SAN/history?p=SAN'
-# response = requests.get(url, timeout=10)
-# content = BeautifulSoup(response.content, "html.parser")
-# print(content)
-# 
-# FinanceData = content.find('tr', attrs={"class": "BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)"}).text
-# print (FinanceData)
-# =============================================================================
 #url = 'https://finance.yahoo.com/quote/SAN/history?p=SAN'
-url = 'https://api.scrapingdog.com/scrape?api_key=<your-api-key>&url=https://finance.yahoo.com/quote/AMZN?p=AMZN&.tsrc=fin-srch'
+#url = 'https://api.scrapingdog.com/scrape?api_key=<your-api-key>&url=https://finance.yahoo.com/quote/AMZN?p=AMZN&.tsrc=fin-srch'
+#url = 'https://api.scrapingdog.com/scrape?api_key=5ea541dcacf6581b0b4b4042&url=https://finance.yahoo.com/quote/AMZN?p=AMZN&.tsrc=fin-srch'
+url = 'https://finance.yahoo.com/quote/SAN/history?p=SAN'
 r = requests.get(url).text
 soup = BeautifulSoup(r,"html.parser")
-alldata = soup.find_all("tbody")
+alldata = soup.find_all("tbody", attrs={"data-reactid": "50"})
+print (alldata)
 
 try:
  table1 = alldata[0].find_all("tr")
 except:
  table1=None
- 
-try:
- table2 = alldata[1].find_all("tr")
-except:
- table2 = None
 
 l={}
 u=list()
@@ -42,16 +29,6 @@ for i in range(0,len(table1)):
         table1_td = None
 
     l[table1_td[0].text] = table1_td[1].text
-    u.append(l)
-    l={}
-
-for i in range(0,len(table2)):
-    try:
-        table2_td = table2[i].find_all("td")
-    except:
-        table2_td = None
-
-    l[table2_td[0].text] = table2_td[1].text
     u.append(l)
     l={}
     
