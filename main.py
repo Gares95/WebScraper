e3fca58b74741471bf93c73641a749e6a5f8b33d
Guillermo <guillermo.garcia.espuela@gmail.com>
2020-09-09 20:44:13 +0200

    changed dictionaries for pandas for a better manipulation

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+# for element in dir():
+#     if element[0:2] != "__":
+#         del globals()[element]
+# del element
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 #url = 'https://api.scrapingdog.com/scrape?api_key=5ea541dcacf6581b0b4b4042&url=https://finance.yahoo.com/quote/AMZN?p=AMZN&.tsrc=fin-srch'
 url = 'https://finance.yahoo.com/quote/SAN/history?p=SAN'
 r = requests.get(url).text
@@ -8,20 +13,15 @@ alldata = soup.find_all("tbody", attrs={"data-reactid": "50"})
 print (alldata)
 
 myDict = {}
-for tables in alldata:
-    count = 0
-    #print("table {}, tr: {}". format(tables, tables.find_all("tr"))
-    #print(tables.find_all("tr"))
-    for i in tables.find_all("tr"):
-        count +=1
-        auxTable = i.find_all("td")
-        # Because each parameter for this specific webpage 
-        # contains only one value we can use the indexes 0 and 1 only
-        myDict[auxTable[0].text] = auxTable[1].text
+df = pd.DataFrame(columns=['Date', 'Open Value'])
 
-
-# If we want it in dataFrame format
-# =============================================================================
-# import pandas as pd
-# df = pd.DataFrame(list(myDict.items()), columns = ['column1', 'column2'])
-# =============================================================================
+count = 0
+for i in alldata[0].find_all("tr"):
+    auxTable = i.find_all("td")
+    # Because each parameter for this specific webpage 
+    # contains only one value we can use the indexes 0 and 1 only
+    # myDict[auxTable[0].text] = auxTable[1].text
+    df.loc[count] = [auxTable[0].text, auxTable[1].text]
+    count +=1
+print(myDict)
+print(df)
