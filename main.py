41c61fa2a63aab593f7015ab869f19ce9f87ef2e
Guillermo <guillermo.garcia.espuela@gmail.com>
2020-09-17 13:45:36 +0200

    Used the function give_trend to obtain dataframe with all the companies

--- main.py
+++ main.py
@@ -13,38 +13,69 @@ from date_conversion import date_conversion
 
 
 trends = give_me_trend()
-url = trends.iloc[0,1]
+urls = trends.iloc[:,1]
 
-#url = 'https://api.scrapingdog.com/scrape?api_key=5ea541dcacf6581b0b4b4042&url=https://finance.yahoo.com/quote/AMZN?p=AMZN&.tsrc=fin-srch'
-#url = 'https://finance.yahoo.com/quote/SAN/history?p=SAN'
-r = requests.get(url).text
-soup = BeautifulSoup(r,"html.parser")
-# alldata = soup.find_all("tbody")
-alldata = soup.find_all("tbody", attrs={"data-reactid": "50"})
-print (alldata)
-CompName = soup.find("h1", attrs={"class": "D(ib) Fz(18px)"})
-
-df = pd.DataFrame(columns=['Date', 'Close Value'])
-# Close Value
-count = 0
-for i in alldata[0].find_all("tr"):
-    auxTable = i.find_all("td")
-    # Because each parameter for this specific webpage 
-    # contains only one value we can use the indexes 0 and 1 only
-    df.loc[count] = [auxTable[0].text, auxTable[4].text]
-    count +=1
-
+df = pd.DataFrame(columns=['Date', 'Company'])
+for url in urls:
+    
+    r = requests.get(url).text
+    soup = BeautifulSoup(r,"html.parser")
+    # alldata = soup.find_all("tbody")
+    alldata = soup.find_all("tbody", attrs={"data-reactid": "50"})
+    # print (alldata)
+    CompName = soup.find("h1", attrs={"class": "D(ib) Fz(18px)"})
+    
+    # Close Value
+    count = 0
+    if (df.loc[:,'Date'].empty):
+        for i in alldata[0].find_all("tr"):
+            auxTable = i.find_all("td")
+            # Because each parameter for this specific webpage 
+            # contains only one value we can use the indexes 0 and 1 only
+            
+            # if(df.loc[:,'Date'].empty):    
+            df.loc[count] = [auxTable[0].text, auxTable[4].text]
+            count +=1
+        df.columns= ['Date', CompName.text]
+    else:
+        dfAux = pd.DataFrame(columns=['Date', CompName.text])
+        for i in alldata[0].find_all("tr"):
+            auxTable = i.find_all("td")
+            
+            # if(df.loc[:,'Date'].empty):    
+            dfAux.loc[count] = [auxTable[0].text, auxTable[4].text]
+            count +=1
+        df[CompName.text] = dfAux.iloc[:,1]
+    
 #df2 = df.copy()
-df.iloc[:,1] = df.iloc[:,1].str.replace(',','')
-df.iloc[:,1] = df.iloc[:,1].str.replace('-','') # Remove empty data
-df.iloc[:,1] = pd.to_numeric(df.iloc[:,1], downcast="float")
+for c in range(len(df.columns)-1):
+    df.iloc[:,c+1] = df.iloc[:,c+1].str.replace(',','')
+    df.iloc[:,c+1] = df.iloc[:,c+1].str.replace('-','') # Remove empty data
+    df.iloc[:,c+1] = pd.to_numeric(df.iloc[:,c+1], downcast="float")  
+
+# =============================================================================
+# df.iloc[:,3] = df.iloc[:,3].str.replace(',','')
+# df.iloc[:,1:4] = df.iloc[:,1:4].str.replace('-','') # Remove empty data
+# df.iloc[:,1:4] = pd.to_numeric(df.iloc[:,1], downcast="float")
+# =============================================================================
+
 df.iloc[:,0] = date_conversion(df)
+
 ggplot(df) + \
-    aes(x='Date', y = 'Close Value') + \
+    aes(x='Date', y = df.columns[1]) + \
     geom_path() + \
-    ggtitle(CompName.text) + \
+    ggtitle("Close value") + \
     theme(axis_text_x  = element_text(angle = 90, hjust = 1))
 
+
+
+# =============================================================================
+# ggplot(df) + \
+#     aes(x='Date', y = 'Close Value') + \
+#     geom_path() + \
+#     ggtitle(CompName.text) + \
+#     theme(axis_text_x  = element_text(angle = 90, hjust = 1))
+# =============================================================================
 
 # ===================adjusting axis============================================
 # ggplot(df) + \
