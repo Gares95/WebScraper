2c1419b7e26c8c50afc9486c3e84fe312a4b76c6
Guillermo <guillermo.garcia.espuela@gmail.com>
2020-10-05 11:59:39 +0200

    Improved restructuration of tables

--- main.py
+++ main.py
@@ -9,27 +9,31 @@ import pandas as pd
 from plotnine import *
 
 from give_me_trend import give_me_trend
-from date_conversion import date_conversion
+# from date_conversion import date_conversion
 from give_me_top3Data import give_me_top3data
 
 trends = give_me_trend()
 urls = trends.iloc[:,1]
 top3Table = give_me_top3data(urls)
 
-ggplot(top3Table) + \
-    aes(x='Date', y = top3Table.columns[1]) + \
-    geom_path() + \
-    ggtitle("Close value")
-    
-
 top3TableM = pd.melt(top3Table, id_vars = 'Date', value_vars=[top3Table.columns[1], top3Table.columns[2], top3Table.columns[3]])
 top3TableM
+
+top1 = top3TableM[top3TableM['variable']==top3Table.columns[1]]
+top2 = top3TableM[top3TableM['variable']==top3Table.columns[2]]
+top3 = top3TableM[top3TableM['variable']==top3Table.columns[3]]
+    
+ggplot(top1) + \
+    aes(x='Date', y = 'value') + \
+    geom_path() + \
+    ylab("Close value") + \
+    ggtitle(top1['variable'][0])
 
 ggplot(top3TableM) + \
     aes(x='Date') + \
     geom_line(aes(y=top3TableM.value, color=top3TableM.variable)) + \
     ggtitle("Close value")
-
+    
 # ===================adjusting axis============================================
 # ggplot(df) + \
 #     aes(x='Date', y = 'Close Value') + \
